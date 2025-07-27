2293d153a0c175ae7487ca3076dfe677caef03f3
Tanju BOZOK <tanjubozok@hotmail.com>
2025-07-27 16:33:50 +0300

    handles have been added to the project.

---
+++ src/LighthouseSocial.Application/Features/Lighthouse/GetAllLighthouseHandler.cs
@@ -0,0 +1,34 @@
+﻿using LighthouseSocial.Application.Common;
+using LighthouseSocial.Application.Dtos;
+using LighthouseSocial.Domain.Interfaces;
+
+namespace LighthouseSocial.Application.Features.Lighthouse;
+
+public class GetAllLighthouseHandler
+{
+    private readonly ILighthouseRepository _lighthouseRepository;
+
+    public GetAllLighthouseHandler(ILighthouseRepository lighthouseRepository)
+    {
+        _lighthouseRepository = lighthouseRepository;
+    }
+
+    public async Task<Result<IEnumerable<LighthouseDto>>> HandleAsync()
+    {
+        var lighthouses = await _lighthouseRepository.GetAllAsync();
+        if (lighthouses == null || !lighthouses.Any())
+        {
+            return Result<IEnumerable<LighthouseDto>>.Fail("No lighthouses found.");
+        }
+
+        var listDto = lighthouses.Select(x => new LighthouseDto(
+            x.Id,
+            x.Name,
+            x.CountryId,
+            x.Location.Latitude,
+            x.Location.Longitude
+        ));
+
+        return Result<IEnumerable<LighthouseDto>>.Ok(listDto);
+    }
+}
---
+++ src/LighthouseSocial.Domain/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,12 @@
+﻿using LighthouseSocial.Domain.Entities;
+
+namespace LighthouseSocial.Domain.Interfaces;
+
+public interface IPhotoRepository
+{
+    Task AddAsync(Photo photo);
+    Task<Photo?> GetByIdAsync(Guid id);
+    Task DeleteAsync(Guid id);
+    Task<IEnumerable<Photo>> GetByLighthouseIdAsync(Guid lighthouseId);
+    Task<IEnumerable<Photo>> GetByUserIdAsync(Guid userId);
+}
